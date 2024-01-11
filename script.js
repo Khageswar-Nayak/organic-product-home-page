fa8673a065f91aa5bb199a4000a8fd689a71a836
Khageswar-Nayak <khageswarnayak045@gmail.com>
2024-01-12 00:32:59 +0530

    completed product card

---
+++ script.js
@@ -0,0 +1,9 @@
+function showButton() {
+  var button = document.querySelector(".product_image_button");
+  button.style.display = "block";
+}
+
+function hideButton() {
+  var button = document.querySelector(".product_image_button");
+  button.style.display = "none";
+}
